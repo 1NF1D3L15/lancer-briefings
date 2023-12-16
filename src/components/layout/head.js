4b54fc4582a2fc21ec892fb7b6509b047f3c0ced
1NF1D3L15 <154002132+1NF1D3L15@users.noreply.github.com>
2023-12-16 11:54:59 -0800

    Create head.js

---
+++ src/components/layout/head.js
@@ -0,0 +1,8 @@
+let title = document.title
+window.addEventListener('blur', () => {
+  document.title = 'Come Back Please :('
+})
+
+window.addEventListener('focus', () => {
+  document.title = 'GIS | MISSION BRIEFING'
+})
